375a942b4df3505a629bee38b95079b8e4508e3c
manuel-ruiz-23 <rrmanuel1123@gmail.com>
2019-12-10 20:22:46 -0700

    formulario basico

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
   const [user, setUser] = useState(null)
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  })
 
-  useEffect(() => {
+  function handleChange(e){
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleLogin(){
+    console.log('datos',form)
     async function getLogin(){
       const loginData = {
-        email: "rrmanuel1123@gmail.com",
-        password: "ironmanelmejor"
+        email: form.email,
+        password: form.password
       }
-      console.log('json string',JSON.stringify(loginData))
-      const response = await fetch('/users/login', {
-        method: "POST",
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(loginData)
-      });
-      const myjson = await response.json()
-      setUser(myjson.user)
-      console.log('response', myjson)
+      try {
+        console.log('json string',JSON.stringify(loginData))
+        const response = await fetch('/users/login', {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginData)
+        });
+        const myjson = await response.json()
+        setUser(myjson.user)
+        console.log('response', myjson)
+
+      } catch(error) {
+        console.log('error', error.message)
+      }
     }
     getLogin()
-  }, [])
+  }
 
   return (
     <div className="App">
       <h1>Hola mundo!</h1>
       {user && <h1>{user.name}</h1>}
+      email
+      <input name="email" value={form.email} onChange={handleChange}/>
+      password
+      <input name="password" type="password" value={form.password} onChange={handleChange} />
+      <button onClick={handleLogin}>
+        login
+      </button>
     </div>
   );
 }
